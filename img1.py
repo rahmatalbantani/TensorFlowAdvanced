54cf51d9a18a7143e0018aded7895ffe3d5ba621
reystaynight <3332200109@untirta.ac.id>
2024-02-04 03:13:16 +0700

    add model

--- img1.py
+++ img1.py
@@ -3,19 +3,30 @@ from picamera2 import Picamera2
 import time
 
 picam2 = Picamera2()
-picam2.preview_configuration.main.size = (640,480)
+picam2.preview_configuration.main.size = (640, 480)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
+
+capturing = False
 cpt = 0
 maxFrames = 30
+
 while cpt < maxFrames:
-    im= picam2.capture_array()
-    im=cv2.flip(im,-1)
+    im = picam2.capture_array()
+    im = cv2.flip(im, -1)
     cv2.imshow("Camera", im)
-    cv2.imwrite('/home/pi/sanket/images/arduino_uno_%d.jpg' %cpt, im)
-    cpt += 1
-    if cv2.waitKey(1)==ord('q'):
+
+    key = cv2.waitKey(1)
+
+    if key == ord('p') and not capturing:
+        # Capture an image and save it
+        cv2.imwrite('/home/pipod/Desktop/Project/TensorFlowAdvanced/model/korban_uno_%d.jpg' % cpt, im)
+        print("Image captured and saved.")
+        cpt += 1
+
+    elif key == ord('q'):
         break
+
 cv2.destroyAllWindows()
